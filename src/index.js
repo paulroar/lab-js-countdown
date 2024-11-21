cdfd07addfb1e4cea13360a6794b2b6b9c97bfaa
Paulo Prado <paulroar@hotmail.com>
2024-11-21 20:09:09 +0000

    Submited

--- src/index.js
+++ src/index.js
@@ -8,31 +8,43 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
-
-
+document.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+  document.getElementById("start-btn").innerHTML;
+  document.getElementById("start-btn").disabled = true;
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
-}
-
-
+const timerCount = document.getElementById("time");
+timer = setInterval(() => {
+  if(remainingTime >= 0){
+    timerCount.textContent = remainingTime;
+    remainingTime--;
+  } else {
+    clearInterval(timer);
+    showToast();
+  }},1000);
+ 
+  };
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
   // Your code goes here ...
-
-
-
+document.getElementById("toast");
+toast.classList.add("show");
+setTimeout(() =>{
+  toast.classList.remove("show");
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
   // Your code goes here ...
-
+  toast.addEventListener("click", () => {
+    clearTimeout(setTimeout); // Cancel the auto-hide timeout
+    toast.classList.remove("show"); // Hide the toast immediately
+});
+  
 }
